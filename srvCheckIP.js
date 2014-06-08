a62167abdcafe16e18429cbfb3e90e5704e16c8b
mike <jinglei.chen@dianping.com>
2014-06-08 19:23:40 +0800

    update bind ip addr

--- srvCheckIP.js
+++ srvCheckIP.js
@@ -14,5 +14,5 @@ http.createServer(function(req,res){
     
     res.writeHead(200);
     res.end(ano+'\n');
-}).listen(8081,'127.0.0.1');
+}).listen(8081,'0.0.0.0');
 console.log("Server running at http://127.0.0.1:8081");
