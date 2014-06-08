b80fd0ad2fff4c3fbc7b0a1ed7e487e321e8d866
mike <jinglei.chen@dianping.com>
2014-06-08 19:16:48 +0800

    update port

--- clientCheckIP.js
+++ clientCheckIP.js
@@ -7,7 +7,7 @@ lines.forEach(function(line){
     var vals = line.split(':');
     var ip = vals[0];
     var port = vals[1];
-    var opt = new helper.basic_options(ip,'/107.170.197.198:1337','GET',false,false,{t:'101.228.231.196'},port);
+    var opt = new helper.basic_options(ip,'/107.170.197.198:8081','GET',false,false,{t:'101.228.231.196'},port);
     helper.request_data(opt,null,function(data,args){
 	if(data && data.trim()=="true"){
 	    fs.appendFileSync("v.txt",args[0]+'\r\n');
