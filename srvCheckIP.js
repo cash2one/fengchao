acec95b741219d0458bc2991461bcd0145ed291a
mike <jinglei.chen@dianping.com>
2014-06-08 18:05:18 +0800

    update prot

--- srvCheckIP.js
+++ srvCheckIP.js
@@ -15,4 +15,4 @@ http.createServer(function(req,res){
     res.writeHead(200);
     res.end(ano+'\n');
 }).listen(1337,'127.0.0.1');
-console.log("Server running at http://127.0.0.1:1337");
+console.log("Server running at http://127.0.0.1:8088");
