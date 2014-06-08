643c828b99236c9e937723c3b3528eb8e17087b0
mike <jinglei.chen@dianping.com>
2014-06-08 19:35:05 +0800

    update

--- srvCheckIP.js
+++ srvCheckIP.js
@@ -4,15 +4,20 @@ var url = require('url')
 
 http.createServer(function(req,res){
     var addr = req.socket.remoteAddress;
+    var result = '';
     console.log("remote addr:%s",addr);
-    var ano = false;
     var t = url.parse(req.url,true).query.t;
     console.log("expected addr:%s",t);
-    if(addr == t){
-	ano=true;
+    if(!addr || !t){
+	result = "args null";
+    }
+    else if(addr.trim() == t.trim()){
+	result = "true";
+    }else{
+	result = 'false';
     }
     
     res.writeHead(200);
-    res.end(ano+'\n');
+    res.end(result+'\n');
 }).listen(8081,'0.0.0.0');
 console.log("Server running at http://127.0.0.1:8081");
