6b4f5d38521f552a51495978f626838dbd5aef8e
mike <mike442144@163.com>
2014-06-09 21:06:48 +0800

    update

--- clientCheckIP.js
+++ clientCheckIP.js
@@ -12,12 +12,14 @@ function CheckerClient(){
 
 CheckerClient.prototype.start = function(){
     this.init();
-    this.proxyItems.forEach(that.check);
+    this.check(this.proxyItems[0]);
+    //this.proxyItems.forEach(that.check);
 }
 
 CheckerClient.prototype.init = function(){
     this.proxyItems = fs.readFileSync("ip.txt").toString().split('\n');
-
+    this.verified = [];
+    this.doneCount = 0;
     var args = process.argv.slice(2);
     if(args.length>0){
 	this.clientIP = args[0];
@@ -44,12 +46,17 @@ CheckerClient.prototype.check = function(line){
 
 CheckerClient.prototype.process = function(data,args){
     this.doneCount++;
-    if(data && data.trim()=="true"){
-	this.verified.push(args[0]);
-	console.log(args[0]);
+    if(data){
+	if(data.trim()=="true"){
+	    this.verified.push(args[0]);
+	    console.log(args[0]);
+	}
+	else{
+	    console.log("avaliable but not anonymous");	    
+	}
     }
     else{
-	console.log("unavaliable");
+	console.log("unavaliable.");
     }
     console.log("verified:%d,total:%d,avaliable:%d",this.doneCount,this.proxyItems.length,this.verified.length);
     if(this.doneCount == this.proxyItems.length){
@@ -67,6 +74,7 @@ CheckerClient.prototype.flushToFile = function(){
     console.log("new proxy flushed.");
 }
 
+
 var instance = new CheckerClient();
 var that = instance;
-instance.start();
+exports.CheckerClient = instance;
--- search.js
+++ search.js
@@ -1,10 +1,34 @@
 var fs = require('fs')
 var http = require('http')
 var helper = require('./helpers/webhelper.js')
+var checker = require('./clientCheckIP.js')
 
 function search(){
     this.words=[];
+    this.keywordFile = "words.txt";
+    this.resultFile = "result/linkcount.txt";
+    this.cityCategory = 1;
 }
+
+search.prototype.init = function(){
+    if(fs.existsSync(this.keywordFile)){
+	this.words = fs.readFileSync(this.keywordFile).toString().split('\n').filter(function(line){
+	    if(!line || line=='\r'){
+		return false;
+	    }
+	    return true;
+	}).map(function(line){
+	    return line.replace('\r','');
+	});
+    }
+    console.log("total keywords: %d",this.words.length);
+
+    var args = process.argv.slice(2);
+    if(args.length>0){
+	this.cityCategory = args[0];
+    }
+}
+
 //http://www.baidu.com/s?tn=89040009_4_pg&ie=utf-8&bs=%E9%B2%9C%E8%8A%B1&f=8&rsv_bp=1&rsv_spt=3&wd=%E9%B2%9C%E8%8A%B1
 search.prototype.wget = function(){
     if(!this.words.length){
@@ -14,6 +38,7 @@ search.prototype.wget = function(){
     var word = this.words.pop();
     var encoded = encodeURIComponent(word);
     var query = {ie:'utf-8',bs:encoded,wd:encoded};
+    
     var opt  =new helper.basic_options('www.baidu.com','/s','GET',false,false,query);
     opt.headers['referer']='http://www.baidu.com';
     helper.request_data(opt,null,function(data,args){
@@ -26,29 +51,47 @@ search.prototype.process = function(data,args){
 	console.log("data empty");
 	return;
     }
-    var rightAdCount,adLinkCount;
+    var rightAdCount,adLinkCount,isInBlock=0;
     //var cnt = fs.readFileSync('baidu.tabled.html').toString();
     var m = data.match(/bdfs\d/g);
     rightAdCount = m && m.length/2 || 0;
-
+    
     console.log("advertises of right: %s",rightAdCount);
-
+    
     m = data.match(/>推广</g);
     adLinkCount = m && m.length || 0;
-
+    
+    
     if(!adLinkCount){
-	var r = data.match(/<table.*?<\/table>(?=<br\/>)/g)[0];
-	console.log("advertises os list(with bg): %d",r.match(/<table.*?<\/table>/g).length);
+	m = data.match(/<table.*?<\/table>(?=<br\/>)/g);
+	var r = m && m[0];
+	adLinkCount = r && r.match(/<table.*?<\/table>/g).length || 0;
+	isInBlock = adLinkCount>0?1:0;
+	console.log("advertises os list(with bg): %d",adLinkCount);
     }else{
 	console.log("advertises of list: %s",adLinkCount);
     }
+    this.append(args[0],adLinkCount,rightAdCount,isInBlock);
+    setTimeout(function(){
+	that.wget();
+    },2000);
+}
+
+search.prototype.append = function(word,adLinkCount,rightAdCount,isInBlock){
+    var result = [word,adLinkCount,isInBlock,rightAdCount,this.cityCategory,'\r\n'];
+    fs.appendFile(this.resultFile,result.join());
 }
 
 search.prototype.start = function(){
-    this.words.push("投资");
+    this.init();
+//    this.words.push("投资");
     this.wget();
 }
 
 var instance = new search();
 var that = instance;
+//setInterval(function(){
+//    checker.CheckerClient.start();
+//},50000);
+
 instance.start();
