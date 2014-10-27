113595cf74f0459e1e15561e6272f2f3a834297e
Mike <mike@mike-Aspire-E1-471G.(none)>
2014-10-27 09:38:04 +0800

    update

--- baidu.py
+++ baidu.py
@@ -19,7 +19,7 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 def search(word):
@@ -28,48 +28,81 @@ def search(word):
         driver.find_element_by_id("kw").send_keys(word)
         driver.find_element_by_id("su").click()
     except Exception as e:
+        print "error click"
         pass
+    time.sleep(3)
     i=0
     j=0
-    for i in range(0,9):
-        try:
-            driver.find_element_by_id("bdfs"+i)
-        except Exception as e:
-            print e
-            break
-
-    print i;
-    for j in range(0,9):
-        try:
-            driver.find_element_by_id("300"+j)
-        except Exception as e:
-            print e
-            break
+    regexp = re.compile(r'bdfs\d')
+    elem = driver.find_element_by_xpath("//*")
+    source_code = elem.get_attribute("outerHTML")
+    arrRight = regexp.findall(source_code)
+    i= len(arrRight) / 2
+    #for i in range(0,9):
+    #    try:
+    #        driver.find_element_by_id("bdfs"+str(i))
+    #    except Exception as e:
+    #        #print "error detact right ad"
+    #        break
+    regexp = re.compile(r'[\"|\']300\d[\"\']')
+    arrLeft = regexp.findall(source_code)
+    j = len(arrLeft)/2
+    #print i;
+    #for j in range(0,9):
+    #    try:
+    #        driver.find_element_by_id("300"+str(j))
+    #    except Exception as e:
+            #print "error detact left ad"
+    #        break
     if j==0:
         print "in gray box"
-        for j in range(0,9):
-            try:
-                driver.find_element_by_id("400"+j)
-            except Exception as e:
-                print e
-                break
-    print j
+        try:
+            elems = driver.find_elements_by_css_selector("#content_left > table")
+            j=len(elems)/2
+        except Exception as e:
+            print "error detact left ad"
+        #regexp = re.compile(r'[\"|\']400\d[\"\']')
+        #regexp = re.compile(r'<table.*?<\/table>(?=<br\/>)')
+        #arrLeft = regexp.findall(source_code)
+        #j == len(arrLeft)/2
+        #print j
 
-driver = webdriver.Firefox()
+    f = codecs.open('./result/baidu.txt','a','utf8')
+    f.write(words[word]+","+str(j)+","+str(i)+"\n")
+    f.close()
+    print "%s: %d,%d" %(word,j,i)
+dc=DesiredCapabilities.HTMLUNIT
+test_host =
+test_port = 
+server_url = "http://%s:%s/wd/hub" % (test_host, test_port)
+#driver = webdriver.Firefox()
+driver = webdriver.Remote(
+words = {}
 def main():
-    lines = codecs.open('./5755.txt', 'r', 'utf8').readlines()
+    doneItems = codecs.open("./result/baidu.txt",'r','utf8').readlines()
+    print len(doneItems)
+    done = {}
+    for item in doneItems:
+        word = item.split(',')[0]
+        done[word]=True
+    
+    lines = codecs.open('./Keywords_2014_10_16.txt', 'r', 'utf8').readlines()
     print len(lines)
-    words = {}
+    
     for line in lines:
         line = line.replace('\r','').replace('\n','')
         word = line.split(",")[0]
-        words[word] = line
+        try:
+            x=done[word]
+        except KeyError as e:
+            words[word] = line
         pass
     
     driver.get("http://www.baidu.com");
+    time.sleep(20)
     for (d,x) in words.items():
         search(d)
-        time.sleep(8)
+        time.sleep(6)
         
 if __name__ == '__main__':
     print "start"
